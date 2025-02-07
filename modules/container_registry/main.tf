3e099e167b8b6c36242087cb756c77a8a2110e91
ilya-hontarau <ilyah@spacelift.io>
2025-02-07 16:44:59 +0100

    fix: remove admin_enabled toggle

--- modules/container_registry/main.tf
+++ modules/container_registry/main.tf
@@ -3,16 +3,12 @@ resource "azurerm_container_registry" "self_hosted" {
   resource_group_name = var.azure_resource_group_name
   location            = var.azure_resource_group_location
   sku                 = "Standard"
-  admin_enabled       = true
 }
-
-#
 
 resource "azurerm_container_registry" "self_hosted_public" {
   name                   = "selfhostedpublic${var.seed}"
   resource_group_name    = var.azure_resource_group_name
   location               = var.azure_resource_group_location
   sku                    = "Standard"
-  admin_enabled          = true
   anonymous_pull_enabled = true
 }
