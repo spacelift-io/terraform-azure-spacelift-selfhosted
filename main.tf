350d040f765a0c0c9bd355f0b2d8950c218f2937
ilya-hontarau <ilyah@spacelift.io>
2025-02-10 13:52:46 +0100

    feat: add container storage

--- main.tf
+++ main.tf
@@ -14,3 +14,11 @@ module "container_registry" {
   seed           = random_string.seed.result
   resource_group = azurerm_resource_group.rg
 }
+
+
+module "container_storage" {
+  source         = "./modules/container_storage"
+  seed           = random_string.seed.result
+  resource_group = azurerm_resource_group.rg
+  app_domain     = var.app_domain
+}
---
+++ modules/container_storage/variables.tf
@@ -0,0 +1,15 @@
+variable "seed" {
+  type = string
+}
+
+variable "app_domain" {
+  type = string
+}
+
+variable "resource_group" {
+  type = object({
+    name: string
+    location: string
+  })
+  description = "Azure resource group that will be used for the registries."
+}
