9a3a676e48b11b18c8badf73ddebdaca6ebf0d8c
ilya-hontarau <ilyah@spacelift.io>
2025-02-11 11:33:44 +0100

    fix: move ipv4

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -11,3 +11,16 @@ resource "azurerm_subnet" "spacelift" {
   virtual_network_name = azurerm_virtual_network.spacelift.name
   address_prefixes     = ["10.30.1.0/24"]
 }
+
+resource "azurerm_public_ip" "ipv4" {
+  name                = "ipv4"
+  resource_group_name = var.resource_group.name
+  location            = var.resource_group.location
+  allocation_method   = "Static"
+  ip_version          = "IPv4"
+  sku                 = "Standard"
+
+  tags = {
+    environment = "azure-self-hosted"
+  }
+}
