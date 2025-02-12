f0985276062d0465f2a382923dc637d9d985ea43
ilya-hontarau <ilyah@spacelift.io>
2025-02-12 08:07:58 +0100

    fix: extract k8s vars

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -6,12 +6,12 @@ resource "azurerm_kubernetes_cluster" "self-hosted" {
 
   default_node_pool {
     name           = "default"
-    node_count     = 3
-    vm_size        = "Standard_A2_v2"
+    node_count     = var.node_count
+    vm_size        = var.vm_size
     vnet_subnet_id = var.subnet_id
   }
   network_profile {
-    ip_versions         = ["IPv4"]
+    ip_versions = ["IPv4"]
     network_plugin      = "azure"
     network_plugin_mode = "overlay"
     pod_cidr            = "10.244.0.0/20"
