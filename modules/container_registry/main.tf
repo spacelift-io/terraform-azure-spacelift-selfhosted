8d250c834de2fe0676834f8277cc91d947c230f5
ilya-hontarau <ilyah@spacelift.io>
2025-05-29 11:02:37 +0200

    feat: add containers purge task

--- modules/container_registry/main.tf
+++ modules/container_registry/main.tf
@@ -19,3 +19,57 @@ resource "azurerm_role_assignment" "kube_pull_acr" {
   principal_id                     = var.node_principal_id
   skip_service_principal_aad_check = true
 }
+
+resource "azurerm_container_registry_task" "acr_purge_task_backend_image" {
+  name                    = "acr-purge-backend-image"
+  container_registry_id   = azurerm_container_registry.self_hosted.id
+  platform {
+    os = "Linux"
+  }
+
+  encoded_step {
+    task_content = base64encode(<<-EOF
+version: v1.1.0
+steps:
+  - cmd: acr purge --filter 'spacelift-backend:.*' --ago 1d --keep 3
+EOF
+    )
+    context_path = "/dev/null"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  timer_trigger {
+    name     = "daily-purge-schedule"
+    schedule = "0 0 * * *"
+  }
+}
+
+resource "azurerm_container_registry_task" "acr_purge_task_launcher_image" {
+  name                    = "acr-purge-launcher-image"
+  container_registry_id   = azurerm_container_registry.self_hosted_public.id
+  platform {
+    os = "Linux"
+  }
+
+  encoded_step {
+    task_content = base64encode(<<-EOF
+version: v1.1.0
+steps:
+  - cmd: acr purge --filter 'spacelift-launcher:.*' --ago 1d --keep 3
+EOF
+    )
+    context_path = "/dev/null"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  timer_trigger {
+    name     = "daily-purge-schedule"
+    schedule = "0 0 * * *"
+  }
+}
