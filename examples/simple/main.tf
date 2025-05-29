6374ff54815f5e0c1a069262380106635505680d
ilya-hontarau <ilyah@spacelift.io>
2025-05-29 13:16:23 +0200

    feat: add random suffix to rg

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -20,10 +20,17 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
+resource "random_string" "seed" {
+  length  = 4
+  special = false
+  upper   = false
+}
+
+
 module "spacelift" {
   source = "../.."
 
   app_domain          = "spacelift.example.com"
   location            = "westeurope"
-  resource_group_name = "self-hosted-v3-tf-testing"
+  resource_group_name = "self-hosted-v3-tf-testing-${random_string.seed.result}"
 }
